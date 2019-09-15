dd9e7d3934cb1148a4730ea8a25209a50b7d8893
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 17:51:53 -0400

    skeleton of bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var token = require('./token.json'); //token file, you need this to bring the bot online
+
+client.on('ready', () => {
+  console.log(`logged in as ${client.user.tag}!`);
+  console.log(`https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot`);
+});
+
+client.login(`${token.token}`);
