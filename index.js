9594e66169de15c25c635fb3e75937e7dbebd274
Jake Marincic <25059103+jmarin217@users.noreply.github.com>
2019-09-16 19:32:37 -0400

    fix

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var token = require('./token.json'); //token file, you need this to bring the bot online
 var config = require('./config.json'); //config file, also necessary (should already be included)
+const { CommandHandler } = require("djs-commands")
+const CH = new CommandHandler({
+  folder: __dirname + '/modules/',
+  prefix: [`${config.prefix}`]
+});
 
 client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}!`);
@@ -9,6 +14,23 @@ client.on('ready', () => {
   client.user.setActivity(`${config.activity}`);
   console.log(`Set activity to "${config.activity}"`);
 });
+
+//command handling start
+client.on("message", (message) => {
+  if(message.channel.type === 'dm') return;
+  if(message.author.type === 'bot') return;
+  let args = message.content.split(" ");
+  let command = args[0];
+  let cmd = CH.getCommand(command);
+  if(!cmd) return;
+
+  try{
+    cmd.run(client,message,args)
+  }catch(e){
+    console.log(e)
+  }
+});
+//command handling end
 
 client.on('error', console.error); //lol this is kind of necessary
 
