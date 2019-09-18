6ee55885e874645f68507073478009ca102b94ab
Jake Marincic <25059103+jmarin217@users.noreply.github.com>
2019-09-18 02:08:12 -0400

    expand mute functionality

--- modules/mute.js
+++ modules/mute.js
@@ -8,9 +8,13 @@ module.exports = class mute {
     }
 
     async run(client, message, args) {
-        let muteRole = message.guild.roles.get(config.muterole);
+        let muteRole = message.guild.roles.get(config.muteRoleID);
         let member = message.mentions.members.first();
-        member.addRole(muteRole).catch(console.error);
-        message.reply(`muted ${member}`);
+        if(message.member.roles.some(r=>[`${config.adminRoleName}`, `${config.modRoleName}`].includes(r.name)) ) {
+            member.addRole(muteRole).catch(console.error);
+            message.reply(`muted ${member}`);
+        } else {
+            message.channel.send("You can't do that");
+        }
     }
 }
