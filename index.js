3032df4b184b3061007d619f2cb5c286dde88eb5
cat <25059103+jmarin217@users.noreply.github.com>
2019-11-03 02:51:24 -0500

    fix formatting

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 #!/usr/bin/env node
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-var token = require('./token.json'); //token file, you need this to bring the bot online
-var config = require('./config.json'); //config file, also necessary (should already be included)
-const { CommandHandler } = require("djs-commands")
+var token = require("./token.json"); // token file, you need this to bring the bot online
+var config = require("./config.json"); // config file, also necessary (should already be included)
+const { CommandHandler } = require("djs-commands");
 const CH = new CommandHandler({
-  folder: __dirname + '/modules/',
+  folder: __dirname + "/modules/",
   prefix: [`${config.prefix}`]
 });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`logged in as ${client.user.tag}!`);
-  console.log(`https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot`);
+  console.log(
+    `https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot`
+  );
   client.user.setActivity(`${config.activity}`);
   console.log(`Set activity to "${config.activity}"`);
 });
 
 // join message
-client.on("guildMemberAdd" , member => {
-    member.guild.channels.get(config.joinChannelID).send(`Everyone welcome <@${member.id}> to the server!`);
+client.on("guildMemberAdd", member => {
+  member.guild.channels
+    .get(config.joinChannelID)
+    .send(`Everyone welcome <@${member.id}> to the server!`);
 });
 
-//command handling start
-client.on("message", (message) => {
-  if(message.channel.type === 'dm') return;
-  if(message.author.type === 'bot') return;
-  let args = message.content.split(" ");
-  let command = args[0];
-  let cmd = CH.getCommand(command);
-  if(!cmd) return;
+// command handling start
+client.on("message", message => {
+  if (message.channel.type === "dm") return;
+  if (message.author.type === "bot") return;
+  const args = message.content.split(" ");
+  const command = args[0];
+  const cmd = CH.getCommand(command);
+  if (!cmd) return;
 
-  try{
-    cmd.run(client,message,args)
-  }catch(e){
-    console.log(e)
+  try {
+    cmd.run(client, message, args);
+  } catch (e) {
+    console.log(e);
   }
 });
-//command handling end
+// command handling end
 
-client.on('error', console.error); //lol this is kind of necessary
+client.on("error", console.error); // lol this is kind of necessary
 
 client.login(`${token.token}`);
--- modules/help.js
+++ modules/help.js
@@ -1,24 +1,24 @@
-var config = require('../config.json');
-const Discord = require('discord.js');
+var config = require("../config.json");
+const Discord = require("discord.js");
 module.exports = class help {
   constructor() {
-    this.name = 'help',
-    this.alias = ['h'],
-    this.usage = `${config.prefix}help`
+    (this.name = "help"),
+      (this.alias = ["h"]),
+      (this.usage = `${config.prefix}help`);
   }
 
   async run(client, message, args) {
-    var helpConf = require('./comConfig/help.json')
-    let com = args[1];
+    var helpConf = require("./comConfig/help.json");
+    const com = args[1];
     const notFound = new Discord.RichEmbed()
-    .setColor('#D00B00')
-    .setTitle('Help')
-    .addField('List', `${helpConf.list}`);
+      .setColor("#D00B00")
+      .setTitle("Help")
+      .addField("List", `${helpConf.list}`);
     const helpEmbed = new Discord.RichEmbed()
-      .setColor('#D00B00')
-      .setTitle('Help')
+      .setColor("#D00B00")
+      .setTitle("Help")
       .addField(`${com}`, `${helpConf[com]}`);
-    if(helpConf[com] === undefined) return message.channel.send(notFound);
-    if(helpConf[com] != undefined) return message.channel.send(helpEmbed);
+    if (helpConf[com] === undefined) return message.channel.send(notFound);
+    if (helpConf[com] != undefined) return message.channel.send(helpEmbed);
   }
-}
+};
