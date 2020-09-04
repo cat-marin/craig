565070d0084440efc2059e5f831fa57efdd1f7c8
cat <25059103+cat-marin@users.noreply.github.com>
2020-09-04 01:22:25 -0400

    add support warning

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ client.on("guildMemberAdd", member => {
     var finalmessage = message.replace(/\$n/g, member.user.toString())
     finalmessage = finalmessage.replace(/\$p/g, member.displayName.toString())
     member.guild.channels.cache.get(config.joinChannelID).send(finalmessage)
+    member.guild.channels.cache.get(config.joinChannelID).send(`Be sure to use <#624069649469800513> for support, and read the required reading to be verified.`)
 });
 
 // command handling start
